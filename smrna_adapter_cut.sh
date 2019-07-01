c5dae7d78d10a90719b7de670807c39c8859a3dc
ConYel <yeles.konstantinos@gmail.com>
2019-07-01 12:31:02 +0300

    Removed --untrimmed-output
    Use multi-thread  cutadapt

--- smrna_adapter_cut.sh
+++ smrna_adapter_cut.sh
@@ -1,6 +1,6 @@
 # RNA PCR Primer, Index 1 (RPI1)
-# 5’ CAAGCAGAAGACGGCATACGAGATCGTGATGTGACTGGAGTTCCTTGGCACCCGAGAATTCCA 
-# 
+# 5’ CAAGCAGAAGACGGCATACGAGATCGTGATGTGACTGGAGTTCCTTGGCACCCGAGAATTCCA
+#
 # grep -e "TGGAATTCTCGGG" test.pos.sam --color
 # grep -e "ACCCGAGAATTCCA" test.neg.sam --color
 
@@ -23,7 +23,7 @@ fi
 if [ ! -s "${FASTQ}" ]; then
   echo "ERROR: ${FASTQ} not found";
   exit 1
-fi 
+fi
 
 #>illumina_smrna_1.0_3p
 smrna_1_0=TCGTATGCCGTCTTCTGCTTG
@@ -51,13 +51,22 @@ fi
 log=${FASTQ}.cutadapt_log
 >${log}
 trimmed=${FASTQ/.fastq.gz/.trimmed.fastq.gz} # compressed output
-untrimmed=${FASTQ/.fastq/.untrimmed.fastq} 
+untrimmed=${FASTQ/.fastq/.untrimmed.fastq}
 untrimmed=/dev/null # discard untrimmed reads
 adapterOptionA="-a ${smrna_1_0} -a ${smrna_1_5} -a ${smrna_truseq} -a ${smrna_ribo}"
-${CUTADAPT} ${adapterOptionA} -o ${trimmed}  --untrimmed-output ${untrimmed} -O 6 -m 14 -n 8 "${FASTQ}"  | tee -a ${log}
+
+# trimming parameters
+MIN_TRIMMED_READ_LEN=14
+MAX_ADAPTER_ERROR=0.06
+MIN_ADAPTER_MATCH=6
+
+#${CUTADAPT} ${adapterOptionA} -o ${trimmed}  --untrimmed-output ${untrimmed} -O 6 -m 14 -n 8 "${FASTQ}"  | tee -a ${log}
+# removed --untrimmed-output option because cutadapt can't run on multiple threads
+#Multi-core Cutadapt can only write to output files given by -o and -p.
+#This implies that the following command-line arguments are not compatible with multi-core: --untrimmed-output 
+
+${CUTADAPT} ${adapterOptionA} -o ${trimmed} -O ${MIN_ADAPTER_MATCH} -m ${MIN_TRIMMED_READ_LEN} -e ${MAX_ADAPTER_ERROR} -n 8 "${FASTQ}"  | tee -a ${log}
 
 echo "Trimmed reads: ${trimmed}"
 echo "Untrimmed reads: ${untrimmed}"
 echo "Log: ${log}"
-
-
